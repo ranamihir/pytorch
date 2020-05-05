02f4126665e1d7e93039e5455df81ecb17204610
Mihir Rana <mrana@gitlab.dev.tripadvisor.com>
2020-05-06 01:00:01 +0530

    Using existing torch function.

--- torch/nn/utils/clip_grad.py
+++ torch/nn/utils/clip_grad.py
@@ -3,60 +3,61 @@ import torch
 from torch._six import inf
 
 
-def clip_grad_and_get_total_norm_(parameters, max_norm=None, norm_type=2):
-    r"""Gets the total gradient norm of an iterable of parameters.
-    If ``max_norm`` is provided, clips the gradient norm of the parameters in-place.
-
+def clip_grad_norm_(parameters, max_norm, norm_type=2):
+    r"""Clips gradient norm of an iterable of parameters.
     The norm is computed over all gradients together, as if they were
-    concatenated into a single vector.
-
+    concatenated into a single vector. Gradients are modified in-place.
     Arguments:
         parameters (Iterable[Tensor] or Tensor): an iterable of Tensors or a
             single Tensor that will have gradients normalized
         max_norm (float or int): max norm of the gradients
-            If provided, clips the gradients at this value
         norm_type (float or int): type of the used p-norm. Can be ``'inf'`` for
             infinity norm.
-
     Returns:
         Total norm of the parameters (viewed as a single vector).
     """
     if torch.is_tensor(parameters):
         parameters = [parameters]
     parameters = list(filter(lambda p: p.grad is not None, parameters))
+    max_norm = float(max_norm)
     norm_type = float(norm_type)
     if norm_type == inf:
         total_norm = max(p.grad.detach().abs().max() for p in parameters)
     else:
         total_norm = torch.norm(torch.stack([torch.norm(p.grad.detach(), norm_type) for p in parameters]), norm_type)
-
-    # Clip gradients if required
-    if max_norm is not None:
-        max_norm = float(max_norm)
-        clip_coef = max_norm / (total_norm + 1e-6)
-        if clip_coef < 1:
-            for p in parameters:
-                p.grad.detach().mul_(clip_coef)
+    clip_coef = max_norm / (total_norm + 1e-6)
+    if clip_coef < 1:
+        for p in parameters:
+            p.grad.detach().mul_(clip_coef)
     return total_norm
 
 
-def clip_grad_norm_(parameters, max_norm, norm_type=2):
+def clip_grad_norm(parameters, max_norm, norm_type=2):
     r"""Clips gradient norm of an iterable of parameters.
+    .. warning::
+        This method is now deprecated in favor of
+        :func:`torch.nn.utils.clip_grad_norm_`.
+    """
+    warnings.warn("torch.nn.utils.clip_grad_norm is now deprecated in favor "
+                  "of torch.nn.utils.clip_grad_norm_.", stacklevel=2)
+    return clip_grad_norm_(parameters, max_norm, norm_type)
 
-    The norm is computed over all gradients together, as if they were
-    concatenated into a single vector. Gradients are modified in-place.
 
+def clip_grad_value_(parameters, clip_value):
+    r"""Clips gradient of an iterable of parameters at specified value.
+    Gradients are modified in-place.
     Arguments:
         parameters (Iterable[Tensor] or Tensor): an iterable of Tensors or a
             single Tensor that will have gradients normalized
-        max_norm (float or int): max norm of the gradients
-        norm_type (float or int): type of the used p-norm. Can be ``'inf'`` for
-            infinity norm.
-
-    Returns:
-        Total norm of the parameters (viewed as a single vector).
+        clip_value (float or int): maximum allowed value of the gradients.
+            The gradients are clipped in the range
+            :math:`\left[\text{-clip\_value}, \text{clip\_value}\right]`
     """
-    return clip_grad_and_get_total_norm_(parameters, max_norm=max_norm, norm_type=norm_type)
+    if torch.is_tensor(parameters):
+        parameters = [parameters]
+    clip_value = float(clip_value)
+    for p in filter(lambda p: p.grad is not None, parameters):
+        p.grad.data.clamp_(min=-clip_value, max=clip_value)
 
 
 def total_grad_norm(parameters, norm_type=2):
@@ -74,35 +75,4 @@ def total_grad_norm(parameters, norm_type=2):
     Returns:
         Total norm of the parameters (viewed as a single vector).
     """
-    return clip_grad_and_get_total_norm_(parameters, norm_type=norm_type)
-
-
-def clip_grad_norm(parameters, max_norm, norm_type=2):
-    r"""Clips gradient norm of an iterable of parameters.
-
-    .. warning::
-        This method is now deprecated in favor of
-        :func:`torch.nn.utils.clip_grad_norm_`.
-    """
-    warnings.warn("torch.nn.utils.clip_grad_norm is now deprecated in favor "
-                  "of torch.nn.utils.clip_grad_norm_.", stacklevel=2)
-    return clip_grad_norm_(parameters, max_norm, norm_type)
-
-
-def clip_grad_value_(parameters, clip_value):
-    r"""Clips gradient of an iterable of parameters at specified value.
-
-    Gradients are modified in-place.
-
-    Arguments:
-        parameters (Iterable[Tensor] or Tensor): an iterable of Tensors or a
-            single Tensor that will have gradients normalized
-        clip_value (float or int): maximum allowed value of the gradients.
-            The gradients are clipped in the range
-            :math:`\left[\text{-clip\_value}, \text{clip\_value}\right]`
-    """
-    if torch.is_tensor(parameters):
-        parameters = [parameters]
-    clip_value = float(clip_value)
-    for p in filter(lambda p: p.grad is not None, parameters):
-        p.grad.data.clamp_(min=-clip_value, max=clip_value)
+    return clip_grad_norm_(parameters, max_norm=inf, norm_type=norm_type)
