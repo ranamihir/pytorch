8a1f83d82903cf44b4051a252843501155b0d67c
Mihir Rana <mrana@gitlab.dev.tripadvisor.com>
2020-05-06 01:04:15 +0530

    Fixed docstring.

--- torch/nn/utils/clip_grad.py
+++ torch/nn/utils/clip_grad.py
@@ -5,14 +5,17 @@ from torch._six import inf
 
 def clip_grad_norm_(parameters, max_norm, norm_type=2):
     r"""Clips gradient norm of an iterable of parameters.
+
     The norm is computed over all gradients together, as if they were
     concatenated into a single vector. Gradients are modified in-place.
+
     Arguments:
         parameters (Iterable[Tensor] or Tensor): an iterable of Tensors or a
             single Tensor that will have gradients normalized
         max_norm (float or int): max norm of the gradients
         norm_type (float or int): type of the used p-norm. Can be ``'inf'`` for
             infinity norm.
+
     Returns:
         Total norm of the parameters (viewed as a single vector).
     """
@@ -34,6 +37,7 @@ def clip_grad_norm_(parameters, max_norm, norm_type=2):
 
 def clip_grad_norm(parameters, max_norm, norm_type=2):
     r"""Clips gradient norm of an iterable of parameters.
+
     .. warning::
         This method is now deprecated in favor of
         :func:`torch.nn.utils.clip_grad_norm_`.
@@ -45,7 +49,9 @@ def clip_grad_norm(parameters, max_norm, norm_type=2):
 
 def clip_grad_value_(parameters, clip_value):
     r"""Clips gradient of an iterable of parameters at specified value.
+
     Gradients are modified in-place.
+
     Arguments:
         parameters (Iterable[Tensor] or Tensor): an iterable of Tensors or a
             single Tensor that will have gradients normalized
